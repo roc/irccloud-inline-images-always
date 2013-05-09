744af3d2cd5aac5456f51efdf0136481a8c11c7b
Ralph Cowling <acastofthousands@gmail.com>
2013-05-09 18:43:22 +0100

    get some stubby files in here

---
+++ inlineimages.js
@@ -0,0 +1,22 @@
+(function(){
+    'use strict';
+    if(typeof cb !== 'undefined' && cb){
+        console.log('inline images should be enabled');
+        cb().enableInlineImages();
+    } else {
+        console.log('could not call cb');
+    }
+})();
+
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Called when the user clicks on the browser action.
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // No tabs or host permissions needed!
+//   console.log('Turning ' + tab.url + ' red!');
+//   chrome.tabs.executeScript({
+//     code: 'document.body.style.backgroundColor="red"'
+//   });
+// });

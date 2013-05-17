d7d3ea359605be6f04fef83cf479a6a9d0a046ca
Ralph Cowling <acastofthousands@gmail.com>
2013-05-17 18:05:14 +0100

    if in doubt, setTimeout

--- inlineimages.js
+++ inlineimages.js
@@ -1,25 +1,44 @@
 function inlineImages(){
     'use strict';
-    alert('hello!');
-    if(typeof cb !== 'undefined' && cb){
-        console.log('inline images should be enabled');
-        cb().enableInlineImages();
+    if(typeof window.SESSION.currentBuffer !== 'undefined'){
+        try{
+            setTimeout(function(){
+                window.SESSION.currentBuffer.enableInlineImages();
+                console.log('[inlineImages] inline images should be enabled');
+            },2000); // some can't figure out which resource it is that's missing if we don't wrap this in a stupid wait timeout...
+        } catch (e) {
+            console.log('[inlineImages] hmm, bit of a problem',e.message);
+        }
     } else {
-        console.log('could not call cb');
+        console.log('[inlineImages] bah, could not call cb',window.SESSION.currentBuffer,'whut');
     }
 }
 
-inlineImages();
+//Shamelessly culled from https://github.com/avidal/irccloud-colornicks/blob/master/src/colornicks.user.js
+function inject(fn) {
+    'use strict';
 
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+    function waitloop(fn) {
+        var has_cb = typeof(window.SESSION)!== 'undefined' && typeof(window.SESSION.currentBuffer) !== 'undefined';
 
-// Called when the user clicks on the browser action.
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   // No tabs or host permissions needed!
-//   console.log('Turning ' + tab.url + ' red!');
-//   chrome.tabs.executeScript({
-//     code: 'document.body.style.backgroundColor="red"'
-//   });
-// });
+        if(has_cb === false) {
+            console.log('[inlineImages] Resources are not ready...');
+            window.setTimeout(function() { waitloop(fn); }, 100);
+            return;
+        }
+
+        console.log('[inlineImages] Required resources are ready, calling plugin function.');
+        fn();
+    }
+
+    var wrap = '(' + fn.toString() + ')';
+
+    console.log('[inlineImages] Injecting wrapper script.');
+    var script = document.createElement('script');
+    script.textContent += '(' + waitloop.toString() + ')(' + wrap + ');';
+    document.body.appendChild(script);
+    console.log('[inlineImages] Done injecting wrapper script.');
+
+}
+
+inject(inlineImages);
